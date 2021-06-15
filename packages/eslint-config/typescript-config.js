ecd73ea818750cdfaf7d7a42d75f9547315f6088
Ross Reicks <rreicks@tractorzoom.com>
2021-06-15 15:59:28 -0500

    fix: add eslint typescript sort keys errors

--- packages/eslint-config/typescript-config.js
+++ packages/eslint-config/typescript-config.js
@@ -8,12 +8,15 @@ module.exports = {
         ...config.parserOptions,
         project: './tsconfig.json'
     },
+    plugins: ["typescript-sort-keys"],
     rules: {
         ...config.rules,
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/no-floating-promises': 'error',
         '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
         '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-var-requires': 'error'
+        '@typescript-eslint/no-var-requires': 'error',
+        "typescript-sort-keys/interface": "error",
+        "typescript-sort-keys/string-enum": "error"
     }
 }
